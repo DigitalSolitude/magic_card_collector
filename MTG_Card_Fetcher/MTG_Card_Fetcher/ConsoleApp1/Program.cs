143f3fb387f4e38924d477fdad43e40ea4815ce9
digitalsolitude@hotmail.com <digitalsolitude@hotmail.com>
2022-05-26 19:56:50 +0100

    init commit

---
+++ MTG_Card_Fetcher/MTG_Card_Fetcher/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using MtgApiManager.Lib.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTG_Card_Fetcher
+{
+    class Program
+    {
+        static IMtgServiceProvider service = new MtgServiceProvider();
+        static ICardService cardService = service.GetCardService();
+
+        public static void Main()
+        {
+            var res = getCardName("shock");
+            Console.WriteLine(res);
+            Console.WriteLine("Complete");
+        }
+
+        private static async Task<List<string>> getCardName(string cardname)
+        {
+            //return await cardService.Where(x => x.Name == "Shock")
+                                                //.AllAsync();
+
+            var result = await cardService.GetCardTypesAsync();
+            return (List<string>)result;
+        }
+
+    }
+}
