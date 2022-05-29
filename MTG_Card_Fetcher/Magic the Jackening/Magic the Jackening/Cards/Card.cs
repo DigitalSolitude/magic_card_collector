f5522e70cbd9f9d3e60faa6054e4347770164912
digitalsolitude@hotmail.com <digitalsolitude@hotmail.com>
2022-05-29 03:03:51 +0100

    11 tests passing

--- MTG_Card_Fetcher/Magic the Jackening/Magic the Jackening/Cards/Card.cs
+++ MTG_Card_Fetcher/Magic the Jackening/Magic the Jackening/Cards/Card.cs
@@ -3,17 +3,11 @@
     public class Card : ICard
     {
         public string Name { get; }
-
         public List<EManaType> ManaCost { get; }
-
         public ECardType CardType { get; }
-
         public ESubType SubType { get; }
-
         public string OracleText { get; }
-
         public bool isTapped { get; set; }
-
         public Card(string name, List<EManaType> manaValue, ECardType cardType, string oracleText , ESubType subtype = null)
         {
             Name = name;
